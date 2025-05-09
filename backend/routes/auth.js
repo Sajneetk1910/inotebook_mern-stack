c411d1cf148e93127cc1653a77f6da984c5962c1
Sajneetk1910 <your-email@example.com>
2025-05-09 11:26:08 +0530

    first commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+//create user using POST "/api/auth" doesnt require auth
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
+});
+
+module.exports = router;
