2c455dfd257953c176d64db80dc37625aae2f7ed
Sajneetk1910 <your-email@example.com>
2025-05-12 16:39:53 +0530

    thunderclient balidatio collections created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,36 +5,39 @@ const { body, validationResult } = require("express-validator");
 
 //create user using POST "/api/auth" doesnt require auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // if there are erors,return bad requiest and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-
-        res.json({
-          error: "please enter uniques value for email",
-          message: err.message,
-        });
+    // check whether the user with same email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "sorry a user with this already exists" });
+      }
+      // create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
       });
-    res.send(req.body);
+
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured");
+    }
   }
 );
-
 module.exports = router;
-
-
-// video 47
+// video 49
